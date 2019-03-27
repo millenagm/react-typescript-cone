af77efbdab29e0c96f28ad97f138500a9c480e7e
Millena Galvão <millena.gmatos@gmail.com>
2019-03-27 15:33:45 -0300

    template

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />        
+        <h4>cone</h4>
+        <h3>cone</h3>
+        <h2>cone</h2>
+        <h1>cone</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
